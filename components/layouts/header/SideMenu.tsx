53970169f75d251f2737bc3aa57cf88c8bfc6a2c
taipon55 <Taisei127@outlook.com>
2022-12-22 13:15:35 +0900

    header作成

---
+++ components/layouts/header/SideMenu.tsx
@@ -0,0 +1,32 @@
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  useDisclosure,
+  Text,
+} from '@chakra-ui/react';
+
+export const Menu = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button colorScheme="yellow" onClick={onOpen}>
+        open
+      </Button>
+      <Drawer placement="left" isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader>
+            <Text>a</Text>
+            <DrawerBody>
+              <p>記録入力</p>
+            </DrawerBody>
+          </DrawerHeader>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
